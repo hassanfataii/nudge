b359b44b3ae70cbf770bc230831a2cb04c5570e2
hassanfataii <hassanfataii10@gmail.com>
2025-12-31 23:53:43 +0000

    Register service worker as module

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./styles/global.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      await navigator.serviceWorker.register("/sw.js");
+      await navigator.serviceWorker.register("/sw.js", { type: "module" });
     } catch {}
   });
 }
